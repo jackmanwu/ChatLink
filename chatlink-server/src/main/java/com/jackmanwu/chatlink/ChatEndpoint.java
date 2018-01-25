cbf98fe311e16e610f50f144117f970d66bf83cd
wujinlei <wujinlei@cd.tuan800.com>
2018-01-25 18:01:28 +0800

    更改

---
+++ chatlink-server/src/main/java/com/jackmanwu/chatlink/ChatEndpoint.java
@@ -0,0 +1,55 @@
+package com.jackmanwu.chatlink;
+
+import javax.websocket.EncodeException;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * Created by JackManWu on 2018/1/25.
+ */
+@ServerEndpoint("/msgServer")
+public class ChatEndpoint {
+    private Session session;
+
+    private static Set<ChatEndpoint> chatEndpoints = new CopyOnWriteArraySet<ChatEndpoint>();
+
+    private static Map<String, String> users = new HashMap<String, String>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        this.session = session;
+        chatEndpoints.add(this);
+        users.put(session.getId(), username);
+
+        Message message = new Message();
+        message.setFrom(username);
+        message.setContent("Connected!");
+        broadcast(message);
+    }
+
+    @OnMessage
+    public void onMessage(Session session, Message message) {
+        message.setFrom(users.get(session.getId()));
+        broadcast(message);
+    }
+
+    private static void broadcast(Message message) {
+        chatEndpoints.forEach(endpoint -> {
+            synchronized (endpoint) {
+                try {
+                    endpoint.session.getBasicRemote().sendObject(message);
+                } catch (IOException | EncodeException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
