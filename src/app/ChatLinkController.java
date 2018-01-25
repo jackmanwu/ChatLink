41eb15015652732ed1a540f190353be6f1aa4ff3
wujinlei <wujinlei@cd.tuan800.com>
2018-01-25 11:28:48 +0800

    更改

--- src/app/ChatLinkController.java
+++ src/app/ChatLinkController.java
@@ -1,23 +1,51 @@
 package app;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import redis.clients.jedis.Jedis;
+
 
 /**
- * Created by wujinlei on 2018/1/23.
+ * Created by JackManWu on 2018/1/24.
  */
 public class ChatLinkController {
     @FXML
     private TextArea message;
 
     @FXML
-    private TextArea showMsg;
+    private WebView showMessage;
 
     @FXML
-    public void getMessage() {
-        System.out.println("消息: " + message.getText());
-        showMsg.setText(message.getText());
+    private void sendMessage() {
+        String key = "user";
+        String msg = message.getText();
+        if ("".equals(msg)) {
+            return;
+        }
+
+        WebEngine webEngine = showMessage.getEngine();
+        String msgP = "<p><label style='background-color:#98E165;padding:5px 5px;font-size:12;'>" + msg + "</label></p>";
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        if (jedis.exists(key)) {
+            jedis.append(key, msgP);
+        } else {
+            jedis.set(key, msgP);
+        }
+        webEngine.loadContent(jedis.get(key));
+        jedis.close();
+
+        webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
+            @Override
+            public void changed(ObservableValue<? extends Worker.State> observable, Worker.State oldValue, Worker.State newValue) {
+                if(newValue== Worker.State.SUCCEEDED){
+                    webEngine.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+                }
+            }
+        });
     }
 }
--- src/app/MainApp.java
+++ src/app/MainApp.java
@@ -3,11 +3,16 @@ package app;/**
  */
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import redis.clients.jedis.Jedis;
 
 import java.io.IOException;
 
@@ -21,12 +26,27 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        this.stage=primaryStage;
+        this.stage = primaryStage;
         this.stage.setTitle("chatlink");
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(MainApp.class.getResource("view/chatlink.fxml"));
         try {
             VBox vBox = loader.load();
+            WebView webView = (WebView) vBox.getChildren().get(0);
+
+            Jedis jedis = new Jedis("127.0.0.1", 6379);
+            WebEngine webEngine = webView.getEngine();
+            webEngine.loadContent(jedis.get("user"));
+            jedis.close();
+            webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
+                @Override
+                public void changed(ObservableValue<? extends Worker.State> observable, Worker.State oldValue, Worker.State newValue) {
+                    if(newValue== Worker.State.SUCCEEDED){
+                        webEngine.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+                    }
+                }
+            });
+
             Scene scene = new Scene(vBox);
             stage.setScene(scene);
             stage.show();
