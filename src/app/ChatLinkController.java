6fd9f43429a08246f51c54c2d67868460b550dc1
wujinlei <wujinlei@cd.tuan800.com>
2018-01-29 13:17:52 +0800

    更改

--- src/app/ChatLinkController.java
+++ src/app/ChatLinkController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
@@ -70,28 +71,7 @@ public class ChatLinkController implements Initializable {
 
     @FXML
     private void sendMessage() {
-        String msg = message.getText();
-        if ("".equals(msg)) {
-            return;
-        }
-
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                WebEngine webEngine = showMessage.getEngine();
-                webEngine.executeScript("addChildNode(\'" + msg + "\','right')");
-                webEngine.executeScript("window.scrollTo(0,document.body.scrollHeight)");
-            }
-        });
-
-        System.out.println("当前会话：" + session);
-        try {
-            session.getBasicRemote().sendText(msg);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        message.setText("");
+        sendMsg();
     }
 
     @FXML
@@ -102,5 +82,45 @@ public class ChatLinkController implements Initializable {
     @FXML
     private void onMouseExitBtn() {
         button.setStyle("-fx-background-color: #F5F5F5;");
+    }
+
+    @FXML
+    private void EnterSendMsgEvent() {
+        message.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                String msg = message.getText();
+                if ("\n".equals(msg) || "\n\n".equals(msg)) {
+                    message.setText("");
+                } else {
+                    sendMsg();
+                }
+            }
+        });
+    }
+
+    private void sendMsg() {
+        String msg = message.getText();
+        System.out.println("发出的消息：" + msg);
+
+        String resultMsg = msg.replace("\n", "");
+        if ("".equals(resultMsg)) {
+            return;
+        }
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                WebEngine webEngine = showMessage.getEngine();
+                webEngine.executeScript("addChildNode(\'" + resultMsg + "\','right')");
+                webEngine.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+            }
+        });
+
+        try {
+            session.getBasicRemote().sendText(resultMsg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        message.setText("");
     }
 }
